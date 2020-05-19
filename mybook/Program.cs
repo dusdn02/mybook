0b3131b3477d268a8741a881a52a5cac105cddc5
연우 이 <lyw9114@gmail.com>
2020-05-19 11:27:57 +0900

    초기 커밋.

---
+++ mybook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mybook
+{
+    class Program
+    {
+        class Book
+        {
+            public string name;
+            public DateTime issue;
+            public string wirter;
+            public string published;
+            public string publish_place;
+            public string r_edit;
+            public string plan;
+            public string edit;
+            public string design;
+        }
+        static void Main(string[] args)
+        {
+            Book mybook = new Book();
+            mybook.name = "PHP 프로그래밍 입문";
+            mybook.issue
+        }
+    }
+}
